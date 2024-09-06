1969bf054380f014c7ca0ccf8b220115f6b89571
Berro-1 <hussienberro1@gmail.com>
2024-09-06 19:21:38 +0300

    Added CORS middleware

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ import csv
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust based on your setup
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 if __name__ == '__main__':
     import uvicorn
